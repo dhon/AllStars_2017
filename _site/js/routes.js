e7b0cab90a7f6c78f383c5d965481daed294c531
dhon <morgangrimes14@gmail.com>
2017-12-31 17:07:50 -0600

    Relative links

--- _site/js/routes.js
+++ _site/js/routes.js
@@ -5,13 +5,13 @@ angular.module('app.routes', ['ui.router', 'app.controllers', 'app.services'])
 
         .state('home', {
             url: '/',
-            templateUrl: '/Mafia_Stats_2017/home.html',
+            templateUrl: '/home.html',
             controller: 'homeCtrl'
         })
 
         .state('kills', {
             url: '/kills/:id',
-            templateUrl: '/Mafia_Stats_2017/kills.html',
+            templateUrl: '/kills.html',
             controller: 'killsCtrl'
         })
 
